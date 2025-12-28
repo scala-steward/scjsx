2a1b71da1c90a12d49dc0873fd4e7cf0824e6c81
Scala Steward <scala_steward@virtuslab.com>
2025-12-28 15:38:08 +0000

    Update upickle to 4.4.2

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,6 +2,6 @@ object Versions {
   val laminarVersion = "17.2.1"
   val jqueryVersion = "2.1"
   val jqueryDatepickerVersion = "0.10"
-  val upickleVersion = "4.2.1"
+  val upickleVersion = "4.4.2"
   val javaTimeVersion = "2.6.0"
 }
